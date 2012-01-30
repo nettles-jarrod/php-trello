62b7f4c3d51e44590fef62af38b63fce269db0f0
Jarrod Nettles <jarrod@squarecrow.com>
2012-01-30 11:38:17 -0600

    Implemented buildURL method of the Trello client.

--- src/Trello/Client.php
+++ src/Trello/Client.php
@@ -4,8 +4,48 @@ namespace Trello;
 
 class Client
 {
-	public function __construct()
+	protected $apiKey;
+	protected $authToken;
+	protected $authTokenKey;
+	protected $authConsumerKey;
+	protected $authConsumer;
+	
+	public function __construct($apiKey, $apiToken, $apiSecret = null, $tokenSecret = null)
 	{
 		
 	}
+	
+	/**
+	 * Build the request URL for the Trello API call.
+	 * @param string $path
+	 * @param string $query
+	 * @return string
+	 */
+	protected function buildURL($path, $query = '')
+	{
+		$apiURL = 'https://api.trello.com/1' . $path; 
+		
+		if(!empty($this->authTokenKey))
+		{
+			$apiURL .= '?key=' . $this->authTokenKey;
+			$apiURL .= '&token='.$this->authConsumerKey;
+		}
+		else
+		{
+			$apiURL .= '?key=' . $this->apiKey;
+			$apiURL .= '&token=' . $this->authToken;
+		}
+		
+		if(strlen($query) > 0)
+		{
+			$apiURL .= '&' . urlencode($query);
+		}
+		
+		return $apiURL;
+	}
+	
+	public function listBoards()
+	{
+		
+	}	
 }
